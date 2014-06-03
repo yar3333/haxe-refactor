61c17327b198a0e8114d5d56d6b2f15b5d98fb3f
Yaroslav <yar3333@gmail.com>
2014-06-03 12:45:53 +0400

    * makefiles

--- library/make.cmd
+++ library/make.cmd
@@ -1,3 +1,8 @@
+@IF NOT EXIST ..\examples GOTO NO_EXAMPLES
+@del examples.zip 2>NUL
+7z a -tzip examples.zip ..\examples
+:NO_EXAMPLES
+
 @del library.zip 2>NUL
 7z a -tzip library.zip * -xr!make.cmd -xr!library.zip -xr!ndll\Windows\*.exp -xr!ndll\Windows\*.lib
 haxelib submit library.zip
