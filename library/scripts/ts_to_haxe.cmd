3a2a43f51f51b820ea3af97fd493541b096762ba
Yaroslav Sivakov <yar3333@gmail.com>
2014-09-30 20:05:58 +0400

    + extract command & reindent/shift option

---
+++ library/scripts/ts_to_haxe.cmd
@@ -0,0 +1,12 @@
+@echo off
+
+IF [%2]==[] GOTO :help
+
+haxelib run refactor convert --exclude-string-literals --exclude-comments "%1" *.ts "%2" /[.]ts$/.hx/ ts_to_haxe.rules
+goto exit
+
+:help
+echo Using: %~n0 ^<srcDir^> ^<destDir^>
+echo.
+
+:exit
