81b3ddbbcd31c1cfde6421f45c1e25797e6b0f8e
Yaroslav Sivakov <yar3333@gmail.com>
2014-10-03 15:38:18 +0400

    * major refactoring + "override" command

---
+++ run/src/Regexs.hx
@@ -0,0 +1,13 @@
+class Regexs
+{
+	static inline var OVERLOAD = "[ \t]*@[:]overload[^\n]+";
+	static inline var FUNC_PREFIX = "(?:(?:public|private)\\s+)?(?:override\\s+)?";
+	
+	public static inline var ID = "[_a-zA-Z][_a-zA-Z0-9]*";
+	
+	
+	public static inline var OVERLOADS = "(?:" + OVERLOAD + "[\n]+)*";
+	
+	//                                                   1            2                                           3=func name     4=(params):ret
+	public static inline var FULL_FUNC_DECL_TEMPLATE = "(" + OVERLOADS + ")([ \t]*)" + FUNC_PREFIX + "function\\s+({ID})(\\s*\\([^)]*\\)\\s*[:]\\s*" + ID + ")";
+}
