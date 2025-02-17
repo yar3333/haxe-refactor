5c8e5ac09f16e436d3ed6b71369b0957146f9d19
Yaroslav Sivakov <yar3333@gmail.com>
2025-02-17 23:39:19 +0300

    * cmd to build exe

---
+++ build-dist.cmd
@@ -0,0 +1,21 @@
+IF EXIST "dist" (
+	rmdir /s /q dist
+)
+
+mkdir dist
+
+nekotools boot library\run.n
+IF NOT EXIST "library\run.exe" (
+	exit /b 2
+)
+
+mv library\run.exe dist\refactor.exe
+xcopy library\*.ndll dist
+
+xcopy %NEKOPATH%\gcmt-dll.dll dist
+xcopy %NEKOPATH%\neko.dll dist
+xcopy %NEKOPATH%\regexp.ndll dist
+xcopy %NEKOPATH%\std.ndll dist
+
+echo D|xcopy /E library\rules dist\rules
+echo D|xcopy /E library\scripts dist\scripts
